ba74a64b719259817dec1a8242bff921925a93d4
serhii721 <58291816+serhii721@users.noreply.github.com>
2024-10-18 19:54:23 +0300

    feat: pause menu, exit button

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject gameUI;
+    public GameObject pauseUI;
+    public CanvasGroup fadeCanvasGroup;
+    public float fadeDuration = 1f;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseUI.SetActive(false);
+        fadeCanvasGroup.alpha = 0f;
+    }
+
+    // Method is triggered on button click
+    public void TogglePause()
+    {
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Resume()
+    {
+        StartCoroutine(UnfadeFromBlack());
+        ToggleScreenUIs();
+        Time.timeScale = 1f; // Starting time
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        StartCoroutine(FadeToBlack());
+        ToggleScreenUIs();
+        Time.timeScale = 0f; // Stoping time
+        isPaused = true;
+    }
+
+    // Animation of screen fading
+    IEnumerator FadeToBlack()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            fadeCanvasGroup.alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            elapsedTime += Time.unscaledDeltaTime; // using 'unscaledDeltaTime' to work while timeScale = 0
+            yield return null;
+        }
+        fadeCanvasGroup.alpha = 1f;
+    }
+
+    // Animation of screen unfading
+    IEnumerator UnfadeFromBlack()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            fadeCanvasGroup.alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            elapsedTime += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        fadeCanvasGroup.alpha = 0f;
+    }
+
+    // Method for switching 'Pause' and 'Game' UIs
+    private void ToggleScreenUIs()
+    {
+        gameUI.SetActive(!gameUI.activeSelf);
+        pauseUI.SetActive(!pauseUI.activeSelf);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        // For testing Unity editor
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
